f52061dba58c8e46c78aa9802add5ffde241d381
Bennett Juwvipart <benjuw@gmail.com>
2019-10-03 19:45:23 -0700

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -2,7 +2,7 @@ import cv2
 
 key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(0)
-while True:
+#while True:
 
     check, frame = webcam.read()
     print(check) #prints true as long as the webcam is running
