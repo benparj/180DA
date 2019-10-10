3f1962e095c5d8f8ff611c6587709c82b06e6a13
Bennett Juwvipart <benjuw@gmail.com>
2019-10-10 14:54:07 -0700

    Update serverTest.py

--- serverTest.py
+++ serverTest.py
@@ -4,7 +4,7 @@ serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
-conn, addr = serv.accept()
+conn,   addr = serv.accept()
 from_client = ''
 while True:
 data = conn.recv(4096)
