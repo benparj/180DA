5cd0218d629dad9361347122ad73f74e318057df
Bennett Juwvipart <benjuw@gmail.com>
2019-10-10 18:56:27 -0700

    Update serverTest.py

--- serverTest.py
+++ serverTest.py
@@ -4,14 +4,14 @@ serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
-    conn, addr = serv.accept()
-    from_client = ''
-    while True:
-        data = conn.recv(4096)
-        if not data: break
-        from_client += data.decode()
-        print(from_client)
-        conn.send("I am SERVER\n".encode())
-    conn.close()
-    print('client disconnected')
+conn, addr = serv.accept()
+from_client = ''
+while True:
+data = conn.recv(4096)
+if not data: break
+from_client += data.decode()
+print(from_client)
+conn.send("I am SERVER\n".encode())
+conn.close()
+print('client disconnected')
 
